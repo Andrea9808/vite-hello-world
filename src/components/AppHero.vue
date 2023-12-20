0630eb4302bec7f1a82774f130de98166f4f3300
Andrea9808 <andrea.manigrasso98@gmail.com>
2023-12-20 16:14:01 +0100

    feat: aggiunta img

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    name: "AppHero"
+}
+</script>
+
+<template>
+
+    <div class="jambo">
+        <img src="../assets/jambo.jpg" alt="jambo">
+    </div>
+
+</template>
+
+<style scoped>
+   
+</style>
